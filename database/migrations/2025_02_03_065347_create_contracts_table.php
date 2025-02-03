4fc3689c618d5a1cabf88abcfa6de47b663e9bbe
ChamzalIsal <93560711+AkanTahu@users.noreply.github.com>
2025-02-03 14:42:55 +0700

    add model contract and university and seeder role

---
+++ database/migrations/2025_02_03_065347_create_contracts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contracts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->integer('qty_student');
+            $table->date('start_period');
+            $table->date('end_period');
+            $table->integer('spt');
+            $table->integer('bupot');
+            $table->integer('faktur');
+            $table->string('purchase_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contracts');
+    }
+};
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        Role::factory()->create([
+            'name' => 'Admin',
+        ]);
+
+        Role::factory()->create([
+            'name' => 'Dosen',
+        ]);
+
+        Role::factory()->create([
+            'name' => 'Mahasiswa',
+        ]);
+    }
+}
