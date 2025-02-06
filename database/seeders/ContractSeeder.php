529275018f30bcbf02c6e830ef5ce938ae16f52b
N00budrag0n <helmiagung2468@gmail.com>
2025-02-06 20:48:12 +0700

    contract seeder

--- database/seeders/ContractSeeder.php
+++ database/seeders/ContractSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Contract;
+use App\Support\Enums\ContractTypeEnum;
 
 class ContractSeeder extends Seeder
 {
@@ -12,6 +14,15 @@ class ContractSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $contract = Contract::create([
+            'university_id' => 1,
+            'contract_type' => ContractTypeEnum::LICENSE->value,
+            'qty_student' => 100,
+            'start_period' => now(),
+            'end_period' => now()->addYears(1),
+            'spt' => 5,
+            'bupot' => 5,
+            'faktur' => 5,
+        ]);
     }
 }
