a6402f9d7bf8129110f489490ecef8f27321dd39
N00budrag0n <helmiagung2468@gmail.com>
2025-02-10 20:19:51 +0700

    password reset message

--- resources/views/emails/password-reset.blade.php
+++ resources/views/emails/password-reset.blade.php
@@ -4,7 +4,7 @@ Your password has been reset. Here are your login credentials:
 
 Email: {{ $email }}
 Password: {{ $password }}
-
+pda
 Please login and change your password for security.
 
 Best regards,
