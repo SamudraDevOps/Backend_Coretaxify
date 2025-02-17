a85ff3b25aef5a4e4a0ddcc6e6e5006526656a31
ChamzalIsal <93560711+AkanTahu@users.noreply.github.com>
2025-02-17 09:33:52 +0700

    fiill model alamat wajib pajak

--- app/Models/AlamatWajibPajak.php
+++ app/Models/AlamatWajibPajak.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class AlamatWajibPajak extends Model
 {
-    //
+    protected $guarded = ['id'];
+
+    protected $fillable = [
+        'account_id',
+        'assignment_users_id',
+        'negara',
+        'jenis_alamat',
+        'detail_alamat',
+        'is_lokasi_disewa',
+        'npwp_pemilik_tempat_sewa',
+        'nama_pemilik_tempat_sewa',
+        'tanggal_mulai_sewa',
+        'tanggal_berakhir_sewa',
+        'tanggal_mulai',
+        'tanggal_berakhir',
+        'kode_kpp',
+        'kpp',
+        'seksi_pengawasan',
+    ];
 }
