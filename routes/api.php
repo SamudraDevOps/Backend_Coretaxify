84a41f44e61889f42e7374bf9a6d70818b0403f4
N00budrag0n <helmiagung2468@gmail.com>
2025-03-12 13:48:44 +0700

    update cors config

--- routes/api.php
+++ routes/api.php
@@ -41,10 +41,6 @@ use App\Http\Controllers\Api\ApiSistemController;
 //     Route::post('login', [ApiAuthController::class, 'login']);
 //     Route::post('logout', [ApiAuthController::class, 'logout'])->middleware('auth:sanctum');
 // });
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With, X-XSRF-TOKEN');
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Expose-Headers: Content-Disposition');
 
 Route::get('routes/download', function () {
     $routeCollection = Route::getRoutes();
