93e4f4ddbfeff3599c66ddd97fa6bca2ef1fb1f7
ChamzalIsal <93560711+AkanTahu@users.noreply.github.com>
2025-04-11 13:26:52 +0700

    cpbav3

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::apiResource('kode-transaksi', ApiKodeTransaksiController::class, ['only' => ['index']]);
+Route::get('kode-transaksi', [ApiKodeTransaksiController::class, 'index']);
 
 Route::get('download/assignment/{assignment}', [ApiAssignmentController::class, 'downloadPublic'])
     ->name('download.assignment.file')
