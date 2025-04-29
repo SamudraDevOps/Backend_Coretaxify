d381f4ef7945e9e9b766f26021b7477c8eeb9a3e
ChamzalIsal <93560711+AkanTahu@users.noreply.github.com>
2025-04-29 15:21:56 +0700

    checkpo spt

--- app/Services/SptPpnService.php
+++ app/Services/SptPpnService.php
@@ -33,73 +33,73 @@ class SptPpnService extends BaseCrudService implements SptPpnServiceInterface {
             ->where('status', FakturStatusEnum::APPROVED->value)
             ->get();
 
-        // $data['kolom_1a2_dpp'] dokumen lain
+        // $data['cl_1a2_dpp'] dokumen lain
 
         $fakturs1a2 = $fakturs->whereIn('kode_transaksi', [4, 5]);
-        $data['kolom_1a2_dpp']      = $fakturs1a2->sum('dpp');
-        $data['kolom_1a2_dpp_lain'] = $fakturs1a2->sum('dpp_lain');
-        $data['kolom_1a2_ppn']      = $fakturs1a2->sum('ppn');
-        $data['kolom_1a2_ppnbm']    = $fakturs1a2->sum('ppnbm');
+        $data['cl_1a2_dpp']      = $fakturs1a2->sum('dpp');
+        $data['cl_1a2_dpp_lain'] = $fakturs1a2->sum('dpp_lain');
+        $data['cl_1a2_ppn']      = $fakturs1a2->sum('ppn');
+        $data['cl_1a2_ppnbm']    = $fakturs1a2->sum('ppnbm');
 
         $fakturs1a3 = $fakturs->where('kode_transaksi', 6);
-        $data['kolom_1a3_dpp']      = $fakturs1a3->sum('dpp');
-        $data['kolom_1a3_dpp_lain'] = $fakturs1a3->sum('dpp_lain');
-        $data['kolom_1a3_ppn']      = $fakturs1a3->sum('ppn');
-        $data['kolom_1a3_ppnbm']    = $fakturs1a3->sum('ppnbm');
+        $data['cl_1a3_dpp']      = $fakturs1a3->sum('dpp');
+        $data['cl_1a3_dpp_lain'] = $fakturs1a3->sum('dpp_lain');
+        $data['cl_1a3_ppn']      = $fakturs1a3->sum('ppn');
+        $data['cl_1a3_ppnbm']    = $fakturs1a3->sum('ppnbm');
 
         $fakturs1a4 = $fakturs->whereIn('kode_transaksi', [1, 9, 10]);
-        $data['kolom_1a4_dpp']      = $fakturs1a4->sum('dpp');
-        $data['kolom_1a4_ppn']      = $fakturs1a4->sum('ppn');
-        $data['kolom_1a4_ppnbm']    = $fakturs1a4->sum('ppnbm');
+        $data['cl_1a4_dpp']      = $fakturs1a4->sum('dpp');
+        $data['cl_1a4_ppn']      = $fakturs1a4->sum('ppn');
+        $data['cl_1a4_ppnbm']    = $fakturs1a4->sum('ppnbm');
 
         $fakturs1a6 = $fakturs->whereIn('kode_transaksi', [2, 3]);
-        $data['kolom_1a6_dpp']      = $fakturs1a6->sum('dpp');
-        $data['kolom_1a6_dpp_lain'] = $fakturs1a6->sum('dpp_lain');
-        $data['kolom_1a6_ppn']      = $fakturs1a6->sum('ppn');
-        $data['kolom_1a6_ppnbm']    = $fakturs1a6->sum('ppnbm');
+        $data['cl_1a6_dpp']      = $fakturs1a6->sum('dpp');
+        $data['cl_1a6_dpp_lain'] = $fakturs1a6->sum('dpp_lain');
+        $data['cl_1a6_ppn']      = $fakturs1a6->sum('ppn');
+        $data['cl_1a6_ppnbm']    = $fakturs1a6->sum('ppnbm');
 
         $fakturs1a7 = $fakturs->where('kode_transaksi', 6);
-        $data['kolom_1a7_dpp']      = $fakturs1a7->sum('dpp');
-        $data['kolom_1a7_dpp_lain'] = $fakturs1a7->sum('dpp_lain');
-        $data['kolom_1a7_ppn']      = $fakturs1a7->sum('ppn');
-        $data['kolom_1a7_ppnbm']    = $fakturs1a7->sum('ppnbm');
+        $data['cl_1a7_dpp']      = $fakturs1a7->sum('dpp');
+        $data['cl_1a7_dpp_lain'] = $fakturs1a7->sum('dpp_lain');
+        $data['cl_1a7_ppn']      = $fakturs1a7->sum('ppn');
+        $data['cl_1a7_ppnbm']    = $fakturs1a7->sum('ppnbm');
 
         $fakturs1a8 = $fakturs->where('kode_transaksi', 8);
-        $data['kolom_1a8_dpp']      = $fakturs1a8->sum('dpp');
-        $data['kolom_1a8_dpp_lain'] = $fakturs1a8->sum('dpp_lain');
-        $data['kolom_1a8_ppn']      = $fakturs1a8->sum('ppn');
-        $data['kolom_1a8_ppnbm']    = $fakturs1a8->sum('ppnbm');
+        $data['cl_1a8_dpp']      = $fakturs1a8->sum('dpp');
+        $data['cl_1a8_dpp_lain'] = $fakturs1a8->sum('dpp_lain');
+        $data['cl_1a8_ppn']      = $fakturs1a8->sum('ppn');
+        $data['cl_1a8_ppnbm']    = $fakturs1a8->sum('ppnbm');
 
-        $data['kolom_1a_jumlah_dpp'] =
-           ($data['kolom_1a1_dpp'] ?? 0)
-         + ($data['kolom_1a2_dpp'] ?? 0)
-         + ($data['kolom_1a3_dpp'] ?? 0)
-         + ($data['kolom_1a4_dpp'] ?? 0)
-         + ($data['kolom_1a5_dpp'] ?? 0)
-         + ($data['kolom_1a6_dpp'] ?? 0)
-         + ($data['kolom_1a7_dpp'] ?? 0)
-         + ($data['kolom_1a8_dpp'] ?? 0)
-         + ($data['kolom_1a9_dpp'] ?? 0);
+        $data['cl_1a_jumlah_dpp'] =
+           ($data['cl_1a1_dpp'] ?? 0)
+         + ($data['cl_1a2_dpp'] ?? 0)
+         + ($data['cl_1a3_dpp'] ?? 0)
+         + ($data['cl_1a4_dpp'] ?? 0)
+         + ($data['cl_1a5_dpp'] ?? 0)
+         + ($data['cl_1a6_dpp'] ?? 0)
+         + ($data['cl_1a7_dpp'] ?? 0)
+         + ($data['cl_1a8_dpp'] ?? 0)
+         + ($data['cl_1a9_dpp'] ?? 0);
 
-        $data['kolom_1a_jumlah_ppn'] =
-           ($data['kolom_1a2_ppn'] ?? 0)
-         + ($data['kolom_1a3_ppn'] ?? 0)
-         + ($data['kolom_1a5_ppn'] ?? 0)
-         + ($data['kolom_1a6_ppn'] ?? 0)
-         + ($data['kolom_1a7_ppn'] ?? 0)
-         + ($data['kolom_1a8_ppn'] ?? 0)
-         + ($data['kolom_1a9_ppn'] ?? 0);
+        $data['cl_1a_jumlah_ppn'] =
+           ($data['cl_1a2_ppn'] ?? 0)
+         + ($data['cl_1a3_ppn'] ?? 0)
+         + ($data['cl_1a5_ppn'] ?? 0)
+         + ($data['cl_1a6_ppn'] ?? 0)
+         + ($data['cl_1a7_ppn'] ?? 0)
+         + ($data['cl_1a8_ppn'] ?? 0)
+         + ($data['cl_1a9_ppn'] ?? 0);
 
-         $data['kolom_1a_jumlah_ppnbm'] =
-           ($data['kolom_1a2_ppnbm'] ?? 0)
-         + ($data['kolom_1a3_ppnbm'] ?? 0)
-         + ($data['kolom_1a5_ppnbm'] ?? 0)
-         + ($data['kolom_1a6_ppnbm'] ?? 0)
-         + ($data['kolom_1a7_ppnbm'] ?? 0)
-         + ($data['kolom_1a8_ppnbm'] ?? 0)
-         + ($data['kolom_1a9_ppnbm'] ?? 0);
+         $data['cl_1a_jumlah_ppnbm'] =
+           ($data['cl_1a2_ppnbm'] ?? 0)
+         + ($data['cl_1a3_ppnbm'] ?? 0)
+         + ($data['cl_1a5_ppnbm'] ?? 0)
+         + ($data['cl_1a6_ppnbm'] ?? 0)
+         + ($data['cl_1a7_ppnbm'] ?? 0)
+         + ($data['cl_1a8_ppnbm'] ?? 0)
+         + ($data['cl_1a9_ppnbm'] ?? 0);
 
-        $data['kolom_1c_dpp'] = $data['kolom_1a_jumlah_dpp'] + ($data['kolom_1b_dpp'] ?? 0);
+        $data['cl_1c_dpp'] = $data['cl_1a_jumlah_dpp'] + ($data['cl_1b_dpp'] ?? 0);
 
         $faktursMasukan = Faktur::where('akun_penerima_id', $data['sistem_id'])
             ->where('masa_pajak', $month)
@@ -108,24 +108,59 @@ class SptPpnService extends BaseCrudService implements SptPpnServiceInterface {
             ->get();
 
         $fakturs2b = $faktursMasukan->whereIn('kode_transaksi', [4, 5]);
-        $data['kolom_2b_dpp']      = $fakturs2b->sum('dpp');
-        $data['kolom_2b_dpp_lain'] = $fakturs2b->sum('dpp_lain');
-        $data['kolom_2b_ppn']      = $fakturs2b->sum('ppn');
-        $data['kolom_2b_ppnbm']    = $fakturs2b->sum('ppnbm');
+        $data['cl_2b_dpp']      = $fakturs2b->sum('dpp');
+        $data['cl_2b_dpp_lain'] = $fakturs2b->sum('dpp_lain');
+        $data['cl_2b_ppn']      = $fakturs2b->sum('ppn');
+        $data['cl_2b_ppnbm']    = $fakturs2b->sum('ppnbm');
 
         $fakturs2c = $faktursMasukan->whereIn('kode_transaksi', [1, 9, 10]);
-        $data['kolom_2c_dpp']      = $fakturs2c->sum('dpp');
-        $data['kolom_2c_ppn']      = $fakturs2c->sum('ppn');
-        $data['kolom_2c_ppnbm']    = $fakturs2c->sum('ppnbm');
+        $data['cl_2c_dpp']      = $fakturs2c->sum('dpp');
+        $data['cl_2c_ppn']      = $fakturs2c->sum('ppn');
+        $data['cl_2c_ppnbm']    = $fakturs2c->sum('ppnbm');
 
         $fakturs2d = $faktursMasukan->whereIn('kode_transaksi', [2, 3]);
-        $data['kolom_2d_dpp']      = $fakturs2d->sum('dpp');
-        $data['kolom_2d_dpp_lain'] = $fakturs2d->sum('dpp_lain');
-        $data['kolom_2d_ppn']      = $fakturs2d->sum('ppn');
-        $data['kolom_2d_ppnbm']    = $fakturs2d->sum('ppnbm');
+        $data['cl_2d_dpp']      = $fakturs2d->sum('dpp');
+        $data['cl_2d_dpp_lain'] = $fakturs2d->sum('dpp_lain');
+        $data['cl_2d_ppn']      = $fakturs2d->sum('ppn');
+        $data['cl_2d_ppnbm']    = $fakturs2d->sum('ppnbm');
 
-        
+         $data['cl_2g_dpp'] =
+           ($data['cl_2a_dpp'] ?? 0)
+         + ($data['cl_2b_dpp'] ?? 0)
+         + ($data['cl_2c_dpp'] ?? 0)
+         + ($data['cl_2d_dpp'] ?? 0);
 
+         $data['cl_2g_ppn'] =
+            ($data['cl_2a_ppn'] ?? 0)
+         + ($data['cl_2b_ppn'] ?? 0)
+         + ($data['cl_2c_ppn'] ?? 0)
+         + ($data['cl_2d_ppn'] ?? 0);
+         + ($data['cl_2e_ppn'] ?? 0);
+
+         $data['cl_2j_dpp'] = ($data['cl_2g_dpp'] ?? 0 ) + ($data['cl_2h_dpp'] ?? 0 ) + ($data['cl_2i_dpp'] ?? 0 );
+
+         $data['cl_3a_ppn'] = ($data['cl_1a2_ppn'] ?? 0) + ($data['cl_1a3_ppn'] ?? 0) + ($data['cl_1a4_ppn'] ?? 0) + ($data['cl_1a5_ppn'] ?? 0);
+         $data['cl_3c_ppn'] = ($data['cl_2g_ppn'] ?? 0);
+         $data['cl_3e_ppn'] = ($data['cl_3a_ppn'] ?? 0) - ($data['cl_3b_ppn'] ?? 0) - ($data['cl_3c_ppn'] ?? 0) - ($data['cl_3c_ppn'] ?? 0);
+         $data['cl_3g_ppn'] = ($data['cl_3e_ppn'] ?? 0) - ($data['cl_3f_ppn'] ?? 0);
+
+         $cl_4_ppn_terutang_dpp = ($data['cl_4_ppn_terutang_dpp'] ?? 0);
+         $data['cl_4_ppn_terutang'] = $cl_4_ppn_terutang_dpp * 0.12;
+
+         $data['cl_6a_ppnbm'] = ($data['cl_1a2_ppnbm'] ?? 0) + ($data['cl_1a3_ppnbm'] ?? 0) + ($data['cl_1a4_ppnbm'] ?? 0) + ($data['cl_1a5_ppnbm'] ?? 0);
+         $data['cl_6c_ppnbm'] = ($data['cl_6a_ppnbm'] ?? 0) - ($data['cl_6b_ppnbm'] ?? 0);
+         $data['cl_6e_ppnbm'] = ($data['cl_6c_ppnbm'] ?? 0) - ($data['cl_6d_ppnbm'] ?? 0);
+         $data['cl_6f_diminta_pengembalian'] = $data['cl_6e_ppnbm'] < 0;
+
+         $data['cl_7c_dpp'] = ($data['cl_7a_dpp'] ?? 0) - ($data['cl_7b_dpp'] ?? 0);
+         $data['cl_7c_ppn'] = ($data['cl_7a_ppn'] ?? 0) - ($data['cl_7b_ppn'] ?? 0);
+         $data['cl_7c_ppnbm'] = ($data['cl_7a_ppnbm'] ?? 0) - ($data['cl_7b_ppnbm'] ?? 0);
+         $data['cl_7c_dpp_lain'] = ($data['cl_7a_dpp_lain'] ?? 0) - ($data['cl_7b_dpp_lain'] ?? 0);
+
+         $data['cl_8c_dpp'] = ($data['cl_8a_dpp'] ?? 0) - ($data['cl_8b_dpp'] ?? 0);
+         $data['cl_8c_ppn'] = ($data['cl_8a_ppn'] ?? 0) - ($data['cl_8b_ppn'] ?? 0);
+         $data['cl_8c_ppnbm'] = ($data['cl_8a_ppnbm'] ?? 0) - ($data['cl_8b_ppnbm'] ?? 0);
+         $data['cl_8c_dpp_lain'] = ($data['cl_8a_dpp_lain'] ?? 0) - ($data['cl_8b_dpp_lain'] ?? 0);
         dd($data);
         return parent::create($data);
     }
