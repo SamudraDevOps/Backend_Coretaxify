1289e68380f527b30904506418ed9358e662a41f
N00budrag0n <helmiagung2468@gmail.com>
2025-05-05 11:25:34 +0700

    dokumennya bupot

--- app/Models/Bupot.php
+++ app/Models/Bupot.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bupot extends Model
 {
-    //
+    protected $guarded = ['id'];
+
+    public function pembuat() {
+        return $this->belongsTo(Sistem::class, 'pembuat_id');
+    }
+
+    public function representatif() {
+        return $this->belongsTo(Sistem::class, 'representatif_id');
+    }
+
+    public function dokumen_bupot() {
+        return $this->hasMany(BupotDokumen::class);
+    }
+
 }
--- app/Models/BupotDokumen.php
+++ app/Models/BupotDokumen.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class BupotDokumen extends Model
 {
-    //
+    protected $guarded = ['id'];
+
+    public function bupot() {
+        return $this->belongsTo(Bupot::class);
+    }
 }
