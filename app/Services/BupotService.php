b8cd518ecb4ffb4fa4a0b839391a1156405fdbae
N00budrag0n <helmiagung2468@gmail.com>
2025-05-07 13:32:10 +0700

    generate nomor_pemotongan

--- app/Services/BupotService.php
+++ app/Services/BupotService.php
@@ -5,9 +5,23 @@ namespace App\Services;
 use Adobrovolsky97\LaravelRepositoryServicePattern\Services\BaseCrudService;
 use App\Support\Interfaces\Repositories\BupotRepositoryInterface;
 use App\Support\Interfaces\Services\BupotServiceInterface;
+use Illuminate\Database\Eloquent\Model;
 
 class BupotService extends BaseCrudService implements BupotServiceInterface {
     protected function getRepositoryClass(): string {
         return BupotRepositoryInterface::class;
+    }
+    
+    public function create(array $data): ?Model {
+        $data['nomor_pemotongan'] = $this->generateNomorPemotongan();
+
+        return parent::create($data);
+    }
+
+    private function generateNomorPemotongan() {
+        $string1 = rand(0000, 9999);
+        $string2 = strtoupper(str()->random(5));
+
+        return $string1 . $string2;
     }
 }
