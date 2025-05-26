c5f9f277a5a628cbfe73f0599eed592803c05f20
N00budrag0n <helmiagung2468@gmail.com>
2025-05-26 15:36:24 +0700

    update bupot objek pajak fields

--- app/Http/Resources/BupotObjekPajakResource.php
+++ app/Http/Resources/BupotObjekPajakResource.php
@@ -13,7 +13,8 @@ class BupotObjekPajakResource extends JsonResource {
             'jenis_pajak' => $this->jenis_pajak,
             'kode_objek_pajak' => $this->kode_objek_pajak,
             'tarif_pajak' => $this->tarif_pajak,
-            'sifat_pajak_penghasilan' => $this->tarif_pajak,
+            'persentase_penghasilan_bersih' => $this->persentase_penghasilan_bersih,
+            'sifat_pajak_penghasilan' => $this->sifat_pajak_penghasilan,
             'created_at' => $this->created_at->toDateTimeString(),
             'updated_at' => $this->updated_at->toDateTimeString(),
         ];
--- database/migrations/2025_05_01_160315_create_SISTEM_BUPOT_bupot_objek_pajaks_table.php
+++ database/migrations/2025_05_01_160315_create_SISTEM_BUPOT_bupot_objek_pajaks_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
         Schema::create('bupot_objek_pajaks', function (Blueprint $table) {
             $table->id();
             $table->enum('tipe_bupot', BupotTypeEnum::toArray());
-            $table->text('nama_objek_pajak');
-            $table->string('jenis_pajak');
-            $table->string('kode_objek_pajak');
-            $table->float('tarif_pajak');
-            $table->string('sifat_pajak_penghasilan');
-            $table->decimal('dasar_pengenaan_pajak')->nullable();
+            $table->text('nama_objek_pajak')->nullable();
+            $table->string('jenis_pajak')->nullable();
+            $table->string('kode_objek_pajak')->nullable();
+            $table->float('tarif_pajak')->nullable();
+            $table->float('persentase_penghasilan_bersih')->nullable();
+            $table->string('sifat_pajak_penghasilan')->nullable();
             $table->timestamps();
         });
     }
