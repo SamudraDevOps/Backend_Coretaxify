65694ee24d0cd0e1883704a8db22df846e2db9b9
ChamzalIsal <93560711+AkanTahu@users.noreply.github.com>
2025-06-24 13:34:48 +0700

    Update SistemTambahanResource.php

--- app/Http/Resources/SistemTambahanResource.php
+++ app/Http/Resources/SistemTambahanResource.php
@@ -14,6 +14,7 @@ class SistemTambahanResource extends JsonResource {
             'tipe_akun' => $this->tipe_akun,
             'alamat_utama_akun' => $this->alamat_utama_akun,
             'email_akun' => $this->email_akun,
+            'negara_asal' => $this->negara_asal,
             'created_at' => $this->created_at->toDateTimeString(),
             'updated_at' => $this->updated_at->toDateTimeString(),
         ];
