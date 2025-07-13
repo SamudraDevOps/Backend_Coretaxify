a0abe8c718321f1ed63288a331360c697eb0f18b
N00budrag0n <helmiagung2468@gmail.com>
2025-07-13 14:16:03 +0700

    update assignment seeder that add exam based assignment

--- database/seeders/AssignmentSeeder.php
+++ database/seeders/AssignmentSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Assignment;
 use Illuminate\Database\Seeder;
+use App\Support\Enums\AssignmentTypeEnum;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class AssignmentSeeder extends Seeder
@@ -17,6 +18,7 @@ class AssignmentSeeder extends Seeder
             'group_id' => 1,
             'user_id' => 2,
             'task_id' => 1,
+            'tipe' => AssignmentTypeEnum::ASSIGNMENT->value,
             'name' => 'tugas dosen',
             'assignment_code' => 'tugasdosen',
             'start_period' => '2024-02-02',
@@ -28,11 +30,36 @@ class AssignmentSeeder extends Seeder
             'group_id' => 2,
             'user_id' => 5,
             'task_id' => 2,
+            'tipe' => AssignmentTypeEnum::ASSIGNMENT->value,
             'name' => 'tugas psc',
             'assignment_code' => 'tugaspsc',
             'start_period' => '2024-02-02',
             'end_period' => '2024-02-03',
         ]);
         $assignment2->users()->attach([5,16]);
+
+        $exam = Assignment::create([
+            'user_id' => 2,
+            'task_id' => 1,
+            'tipe' => AssignmentTypeEnum::EXAM->value,
+            'name' => 'ujian dosen',
+            'assignment_code' => 'ujiandosen',
+            'start_period' => now(),
+            'end_period' => now()->addYears(1),
+            'duration' => 40,
+        ]);
+        $exam->users()->attach([2,3,4]);
+
+        $exampsc = Assignment::create([
+            'user_id' => 5,
+            'task_id' => 2,
+            'tipe' => AssignmentTypeEnum::EXAM->value,
+            'name' => 'ujian psc',
+            'assignment_code' => 'ujianpsc',
+            'start_period' => now(),
+            'end_period' => now()->addYears(1),
+            'duration' => 30,
+        ]);
+        $exampsc->users()->attach([5,16]);
     }
 }
