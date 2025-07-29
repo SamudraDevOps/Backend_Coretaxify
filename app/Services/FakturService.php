7265c6eaca4492391bbd8ef9a4086bddd691e186
ChamzalIsal <93560711+AkanTahu@users.noreply.github.com>
2025-07-29 11:40:50 +0700

    Update FakturService.php

--- app/Services/FakturService.php
+++ app/Services/FakturService.php
@@ -189,6 +189,7 @@ class FakturService extends BaseCrudService implements FakturServiceInterface {
             case IntentEnum::API_GET_FAKTUR_PENERIMA->value:
                 $filters = array_merge($request->query(),
                 [
+                    'status' => FakturStatusEnum::APPROVED->value,
                     'akun_penerima_id' => $sistem->id,
                     'is_draft' => false
                 ]);
@@ -209,7 +210,6 @@ class FakturService extends BaseCrudService implements FakturServiceInterface {
             case IntentEnum::API_GET_FAKTUR_RETUR_MASUKAN->value:
                 $filters = array_merge($request->query(),
                 [
-                    'status' => FakturStatusEnum::APPROVED->value,
                     'akun_penerima_id' => $sistem->id,
                     'is_retur' => true,
                     'is_kredit' => true,
