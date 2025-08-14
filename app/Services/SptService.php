e81dfadc036f33d84863c05989215ec84c60d42d
ChamzalIsal <93560711+AkanTahu@users.noreply.github.com>
2025-08-14 09:15:06 +0700

    Revert "fix: filter published at show sptpph"
    This reverts commit fbebdf034cbc2231c1fb14625f04adb84d00b121.

--- app/Services/SptService.php
+++ app/Services/SptService.php
@@ -1110,7 +1110,6 @@ class SptService extends BaseCrudService implements SptServiceInterface {
         $monthNumber = MonthHelper::getMonthNumber($month);
 
         $bupots = Bupot::where('pembuat_id', $spt->badan_id)
-                ->where('status_penerbitan', 'published')
                 ->whereMonth('masa_awal', $monthNumber)
                 ->whereYear('masa_awal', $spt->masa_tahun)
                 ->get();
