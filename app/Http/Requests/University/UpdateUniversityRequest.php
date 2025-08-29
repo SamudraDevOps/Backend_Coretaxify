caa0fbab9456873eedcf3c009ff31a08eee706ae
N00budrag0n <helmiagung2468@gmail.com>
2025-08-29 17:58:54 +0700

    update university request

--- app/Http/Requests/University/UpdateUniversityRequest.php
+++ app/Http/Requests/University/UpdateUniversityRequest.php
@@ -7,18 +7,7 @@ use Illuminate\Foundation\Http\FormRequest;
 class UpdateUniversityRequest extends FormRequest {
     public function rules(): array {
         return [
-            'nik_anggota_keluarga' => 'nullable|string',
-            'jenis_kelamin' => 'nullable|string',
-            'tempat_lahir' => 'nullable|string',
-            'nomor_kartu_keluarga' => 'nullable|string',
-            'nama_anggota_keluarga' => 'nullable|string',
-            'status_hubungan_keluarga' => 'nullable|string',
-            'pekerjaan' => 'nullable|string',
-            'status_unit_perpajakan' => 'nullable|string',
-            'status_ptkp' => 'nullable|string',
-            'tanggal_lahir' => 'nullable|date',
-            'tanggal_mulai' => 'nullable|date',
-            'tanggal_berakhir' => 'nullable|date',
+            'name' => 'nullable|string'
         ];
     }
 }
