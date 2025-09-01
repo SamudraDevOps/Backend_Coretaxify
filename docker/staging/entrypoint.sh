ec0097f876a1133fe240dd2a5897fb83dd9a4d84
Danang Ardianto <ardhyanto6629@gmail.com>
2025-09-01 14:28:54 +0700

    setup dockerfile

---
+++ docker/staging/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+# Change to the application directory
+cd /var/www/html
+
+echo "Running Laravel startup tasks..."
+
+# IMPORTANT: Re-apply ownership to the www-data user
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
+echo "Startup complete. Handing over to main process..."
+
+# Execute the command passed as arguments to the script (the Dockerfile's CMD)
+exec "$@"
