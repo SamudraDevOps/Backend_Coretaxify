3a985dc0db7b824eeac5eb00923311bdc3bb7ec1
Danang Ardianto <ardhyanto6629@gmail.com>
2025-09-03 15:04:21 +0700

    update entrypoint

--- docker/staging/entrypoint.sh
+++ docker/staging/entrypoint.sh
@@ -7,12 +7,12 @@ cd /var/www/html
 
 echo "Running Laravel startup tasks..."
 
-php artisan optimize
-php artisan migrate --force
-
 # IMPORTANT: Re-apply ownership to the www-data user
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
+php artisan optimize
+php artisan migrate --force
 
 echo "Startup complete. Handing over to main process..."
 
