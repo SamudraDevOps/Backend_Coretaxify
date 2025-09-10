e6853fb987d986e4a19c7b62712a98b0b26039e1
N00budrag0n <helmiagung2468@gmail.com>
2025-09-10 16:26:58 +0700

    directly force https

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,13 +5,12 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
-        commands: __DIR__ . '/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -42,12 +41,4 @@ return Application::configure(basePath: dirname(__DIR__))
             }
         });
         //
-    })->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustProxies(
-            '*',
-            SymfonyRequest::HEADER_X_FORWARDED_FOR
-            | SymfonyRequest::HEADER_X_FORWARDED_HOST
-            | SymfonyRequest::HEADER_X_FORWARDED_PORT
-            | SymfonyRequest::HEADER_X_FORWARDED_PROTO
-        );
     })->create();
