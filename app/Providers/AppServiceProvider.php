596c785e8af80f45d22d9f392ee7887c0adba597
N00budrag0n <helmiagung2468@gmail.com>
2025-09-10 16:38:24 +0700

    change way to force

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,6 +29,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        URL::forceScheme('https');
+        if ($this->app->environment('production')) {
+            $this->app['request']->server->set('HTTPS','on');
+        }
     }
 }
