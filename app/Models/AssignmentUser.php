5dc9ac8947de849acf48d13b3ee173260cf2d114
N00budrag0n <helmiagung2468@gmail.com>
2025-10-10 13:08:36 +0700

    convert to date time

--- app/Models/AssignmentUser.php
+++ app/Models/AssignmentUser.php
@@ -18,7 +18,7 @@ class AssignmentUser extends Model
     protected $guarded = ['id'];
 
     public function isValid() {
-        return Carbon::now();
+        return Carbon::now()->toDateTimeString();
         if ($this->assignment->isExam()) {
             if (!$this->is_start) {
                 return $this->assignment->start_period <= now() && $this->assignment->end_period >= now();
